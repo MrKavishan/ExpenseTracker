331aa78ca46845b7ac6508e053860e1f206b4d1a
Udith Kavishan <140702649+MrKavishan@users.noreply.github.com>
2025-07-03 00:01:11 +0530

    Created model calss and DbContext calss

---
+++ ExpenseTracker/Data/ExpenseAppContext.cs
@@ -0,0 +1,15 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Data
+{
+    public class ExpenseAppContext :DbContext
+    {
+        public ExpenseAppContext(DbContextOptions<ExpenseAppContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Expence> Expences { get; set; } 
+
+    }
+}
---
+++ ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Models
+{
+    public class Expence
+    {
+        public int Id { get; set; }
+        [Required] public string Description { get; set; } = null!;
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Amount Need To Be Higher")]
+        public int Amount { get; set; }
+        [Required] public string Category { get; set; } = null!;
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
